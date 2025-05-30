e2d02ee035013fc7749fd734079adbe4009a8ab6
gatormeek <204446387+gatormeek@users.noreply.github.com>
2025-05-29 22:24:27 -0700

    delete first attempt at a box
    delete first attempt an etch box, add beginnings of a dropdown etch size box

---
+++ script.js
@@ -0,0 +1,7 @@
+
+    for (let i=0; i<=40; i++) {
+        let row = document.createElement("div");
+        for (let j=0; j<=40; i++) {
+            let column = document.createElement("div");
+        }
+    }
