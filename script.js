11e16ba3c84d52952b3a9b1352e6785e37a800e9
gatormeek <204446387+gatormeek@users.noreply.github.com>
2025-06-03 21:20:07 -0700

    functional initial etch, drop-box functionality
    made a 40x40 functional etch pad, and a semi-functional size menu. Does not change the size of the
    etch pad pixels yet, but otherwise aesthetically looks like a size menu.

--- script.js
+++ script.js
@@ -1,7 +1,43 @@
-
-    for (let i=0; i<=40; i++) {
+let makeEtch=function() {
+    for (let i=0; i<10; i++) {
         let row = document.createElement("div");
-        for (let j=0; j<=40; i++) {
-            let column = document.createElement("div");
+        row.style.display="flex";
+        for (let j=0; j<10; j++) {
+            let column = document.createElement("button");
+            column.style.width="25px";
+            column.style.height="25px";
+            column.style.border="none";
+            column.style.padding="0";
+            column.style.margin="0";
+            column.addEventListener('mouseover', () => {
+                column.style.background="red";
+            });
+            row.appendChild(column);
         }
+        document.getElementById("etch-container").appendChild(row);
     }
+    
+}
+makeEtch();
+
+let selectSizeBtn = document.querySelector("#dropbtn");
+let sizeSelections = document.querySelector("#dropdown-choices");
+let selectionHover = document.querySelectorAll("a");
+selectSizeBtn.addEventListener('click', () => {
+    sizeSelections.style.display="flex";
+    sizeSelections.addEventListener('mouseleave', () => {
+        sizeSelections.style.display="none";
+    });
+});
+selectionHover.forEach(link => {
+    link.addEventListener('mouseenter', () => {
+        link.style.color='white';
+        link.style.background='#1da1f2';
+        link.style.border='none';
+    });
+    link.addEventListener('mouseleave', () => {
+        link.style.color='';
+        link.style.background='';
+        link.style.border='';
+    })
+})
